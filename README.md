80631ea154b3f9f703e43cfa0af0e8a0d5b344e4
muzala <pmuzalamwaku@yahoo.com>
2022-02-11 22:07:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-uekv3v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-uekv3v)
---
+++ index.js
@@ -0,0 +1,3 @@
+hello node // run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
